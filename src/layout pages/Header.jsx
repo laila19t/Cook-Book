3a91439fddde90506ffc68f579daf44ec89ae774
laila19t <127900249+laila19t@users.noreply.github.com>
2024-02-08 13:54:09 +0400

    started in responsive adjustments

--- src/layout pages/Header.jsx
+++ src/layout pages/Header.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { FaUser } from "react-icons/fa";
+import { FiMenu } from "react-icons/fi";
+import { slide as Menu } from 'react-burger-menu';
 
 export default function Header(){
     return(
        <div className="header-cont">
         <div style={{display:"flex", alignItems:"center"}}>
+        <button><FiMenu /></button>
+        <Menu>
+      <a className="menu-item" href="/"> Home </a>
+      <a className="menu-item" href="/salads"> Salads </a>
+      <a className="menu-item" href="/pizzas"> Pizzas </a>
+      <a className="menu-item" href="/desserts"> Desserts </a>
+    </Menu>
         <h3 className="logo">CB</h3>
            <div className="links">
             <NavLink to="/">Home</NavLink>
